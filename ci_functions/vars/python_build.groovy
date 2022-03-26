575fc56f64b21ab5df9ea2486197b114b5b081de
akinoFu <71040049+akinoFu@users.noreply.github.com>
2022-03-26 16:14:38 -0700

    Update python_build.groovy

--- ci_functions/vars/python_build.groovy
+++ ci_functions/vars/python_build.groovy
@@ -25,7 +25,7 @@ def call(directoryName, dockerRepoName, imageName) {
             }
             stage('Package') { 
                 when { 
-                    expression { env.GIT_BRANCH == 'origin/main' } 
+                    expression { env.GIT_BRANCH == 'main' } 
                 } 
                 steps { 
                     withCredentials([string(credentialsId: 'akino_dockerhub', variable: 'TOKEN')]) { 
