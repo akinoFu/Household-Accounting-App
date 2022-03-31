b7895b408ca6a1ac8ffa96c38e25fbdd7aa41b16
akinoFu <akino.fu85@outlook.com>
2022-03-30 17:46:54 -0700

    Add config volumes

--- Storage/app.py
+++ Storage/app.py
@@ -15,16 +15,37 @@ from pykafka.common import OffsetType
 from threading import Thread
 import json
 import time
+import os
 
-with open("app_conf.yml", "r") as f:
+if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+    print("In Test Environment")
+    app_conf_file = "/config/app_conf.yml"
+    log_conf_file = "/config/log_conf.yml"
+else:
+    print("In Dev Environment")
+    app_conf_file = "app_conf.yml"
+    log_conf_file = "log_conf.yml"
+
+with open(app_conf_file, 'r') as f:
     app_config = yaml.safe_load(f.read())
-conf_data = app_config["datastore"]
-
-with open("log_conf.yml", "r") as f:
+    
+# External Logging Configuration
+with open(log_conf_file, 'r') as f:
     log_config = yaml.safe_load(f.read())
     logging.config.dictConfig(log_config)
 
 logger = logging.getLogger('basicLogger')
+
+conf_data = app_config["datastore"]
+# with open("app_conf.yml", "r") as f:
+#     app_config = yaml.safe_load(f.read())
+
+
+# with open("log_conf.yml", "r") as f:
+#     log_config = yaml.safe_load(f.read())
+#     logging.config.dictConfig(log_config)
+
+# logger = logging.getLogger('basicLogger')
 
 def outputInfoLog(trace_id, event_name):
     msg = f"Stored event {event_name} request with a trace id of {trace_id}"
